59b39e1793c4562d027a7839dc6cc37cf2ceed04
Jayesh Mate <jayeshmate@Jayeshs-MacBook-Air.local>
2023-06-13 12:42:57 +0530

    .env and gitignore added .

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
 const express = require("express");
 const { connection } = require("./db");
 const { jobRoute } = require("./route/Job.route");
+require("dotenv").config();
 
 const app = express();
 app.use(express.json());
 app.use("/jobs", jobRoute);
 
-app.listen(8080, async () => {
+app.listen(process.env.port, async () => {
     try {
         await connection;
-        console.log("Connected to DB")
+        console.log("Connected to DB");
     } catch (error) {
         console.log("Something went wrong");
     }
